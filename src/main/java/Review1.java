da0d0981f55bd245c9ac609e768de08c9123c630
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-02-03 15:34:03 +0000

    Solve Q1 - Q5

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,57 @@
 public class Review1 {
 
+	public static void main(String args[]) {
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
+
 	public static void q1() {
 		//Write question 1 code here
+
+		//Print message
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+
+		//Assign variables
+		int a = 9;
+		int b = 5;
+
+		//Print quotient and modulo (remainder)
+		System.out.println(a/b);
+		System.out.println(a%b);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+
+		//Write message with new line characters
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		
+		//Literally print the boolean value "true"
+		System.out.println(true);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+
+		//Assign the strings
+		String a= "Hello";
+		String b = "World";
+		
+		//Print the concatenated strings
+		
+		System.out.println(a +' '+ b);
+
 	}
 
 }
